8def9552658808d81da3a03e06974707cf2e5833
Jefferson F3L!X <jeffersonfelizx@gmail.com>
2024-01-31 16:46:48 -0300

    deploy do blogpessoal

---
+++ src/main/java/org/generation/blogpessoal/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package org.generation.blogpessoal.repository;
+
+import org.generation.blogpessoal.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    public Optional<Usuario> findByUsuario(String usuario);
+}
